f16132e292523c3d7426fffc5c3f296794fbf593
root <root@DESKTOP-L3764RE.localdomain>
2025-09-01 21:59:15 -0400

    added word count function to main.py

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def get_word_count(book):
+    words = book.split()
+    return len(words)
+
 def main():
-    return print(get_book_text("books/frankenstein.txt"))
+    #return print(get_book_text("books/frankenstein.txt"))
+    book = get_book_text("books/frankenstein.txt")
+    return print(str(get_word_count(book)) + " words found in the document")
 
 main()
