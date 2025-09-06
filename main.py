cdcf83f2918e83d87aa33029813d5fcd72efba14
root <root@DESKTOP-L3764RE.localdomain>
2025-09-05 21:13:57 -0400

    added user specification to sys bookpath to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from stats import get_word_count, get_char_count, get_report
 
+if len(sys.argv) < 2:
+    sys.exit("Usage: python3 main.py <path_to_book>")
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,10 +10,10 @@ def get_book_text(path):
 
 def main():
     #return print(get_book_text("books/frankenstein.txt"))
-    book = get_book_text("books/frankenstein.txt")
+    book = get_book_text(sys.argv[1])
     word_count = str(get_word_count(book))
     char_count = get_char_count(book)
 
     # return print(str(get_word_count(book)) + " words found in the document"), print(get_char_count(book)), get_report("books/frankenstein.txt",word_count, char_count)
-    return get_report("books/frankenstein.txt",word_count, char_count)
+    return get_report(sys.argv[1],word_count, char_count)
 main()
