07730a97b1cc050472ccbe5b57fb86226ee50a4d
root <root@DESKTOP-L3764RE.localdomain>
2025-09-05 20:50:31 -0400

    added bookbot function to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_word_count, get_char_count
+import sys
+from stats import get_word_count, get_char_count, get_report
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,6 +9,9 @@ def get_book_text(path):
 def main():
     #return print(get_book_text("books/frankenstein.txt"))
     book = get_book_text("books/frankenstein.txt")
-    return print(str(get_word_count(book)) + " words found in the document"), print(get_char_count(book))
+    word_count = str(get_word_count(book))
+    char_count = get_char_count(book)
 
+    # return print(str(get_word_count(book)) + " words found in the document"), print(get_char_count(book)), get_report("books/frankenstein.txt",word_count, char_count)
+    return get_report("books/frankenstein.txt",word_count, char_count)
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,36 @@ def get_char_count(book):
         else:
             dict_count[c] = 1
     return dict_count
+
+def sort_on(items):
+    return items["num"]
+
+def get_report(book_path, word_count, char_count):
+    list = []
+    for i in char_count:
+        appears = char_count[i]
+        if i.isalpha():
+            list.append({"char": i, "num": appears})
+    list.sort(reverse=True, key=sort_on)
+    # report header
+    header = "============ BOOKBOT ============\n"
+    # statement 1
+    pathstat = "Analyzing book found at " + book_path + "...\n"
+    # word header
+    wordhead = "----------- Word Count ----------\n"
+    # statement 2
+    wordstat = "Found " + word_count + " total words\n"
+    # char header
+    charhead = "--------- Character Count -------\n"
+    print(header)
+    print(pathstat)
+    print(wordhead)
+    print(wordstat)
+    print(charhead)
+    for i in list:
+        body = i["char"] + ": " + str(i["num"]) + "\n"
+        print(body)
+    # end header
+    endhead = "============= END ==============="
+    print(endhead)
+    return 
